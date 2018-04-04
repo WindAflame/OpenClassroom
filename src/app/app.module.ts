0c3af61b1c3cf7666bb07009f74a43a6f9ca9dc1
Endy WindAflame <dev.windaflame@gmail.com>
2018-04-04 12:46:55 +0200

    includes all services and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './authentification/signup/signup.component';
+import { SigninComponent } from './authentification/signin/signin.component';
+import { ListComponent } from './book/list/list.component';
+import { DetailComponent } from './book/detail/detail.component';
+import { NewComponent } from './book/new/new.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    ListComponent,
+    DetailComponent,
+    NewComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
